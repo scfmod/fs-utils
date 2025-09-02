16d27c9f65242e4fa18badee529ec5f33f5fa013
scfmod <45406107+scfmod@users.noreply.github.com>
2025-09-02 19:04:20 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 cargo build --release --workspace --exclude fs-unpack
 cargo build --release -p fs-unpack --target i686-pc-windows-msvc
 
-7z a ./build/fs-tools.zip ./target/release/*.exe ./target/i686-pc-windows-msvc/release/fs-unpack.exe ./bin
+7z a ./build/fs-tools.zip ./target/release/*.exe ./target/i686-pc-windows-msvc/release/fs-unpack.exe ./target/release/defarm.dll ./bin
